71ff131583b9f35b2545937590b4c4cbc688e60c
sdv <abarakus@bk.ru>
2022-04-04 10:20:08 +0300

    fixed alert processing

--- src/main/resources/static/distr/js/alert.js
+++ src/main/resources/static/distr/js/alert.js
@@ -1,19 +1,14 @@
 let setStatus = () => {
     $.get("draw-alerts", alerts => {
-        if (!JSON.parse(alerts).length) {
-            resetStatus();
-            return;
-        }
         JSON.parse(alerts).forEach(alert => {
             let row = $('#row_' + alert.patient.id);
             let dot = $('#dot_' + alert.patient.id);
+            row.off();
             if (!alert.id) {
-                row.off();
                 $(dot).removeClass("red").removeClass("blink").addClass("green");
                 return;
             }
             dot.removeClass("green").addClass("red").addClass("blink");
-            row.off();
             row.mousedown(() => row.css('background', '#868686'));
             row.mouseup(() => {
                 $.get("close-alert?id=" + alert.id);
@@ -22,15 +17,6 @@ let setStatus = () => {
             });
         })
     });
-}
-
-let resetStatus = () => {
-    $('.patientRow').each((i, row) => {
-        $(row).off();
-    })
-    $('.dot').each((i, dot) => {
-        $(dot).removeClass("red").removeClass("blink").addClass("green");
-    })
 }
 
 $(document).ready(() => {
